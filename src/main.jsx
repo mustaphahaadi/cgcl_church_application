3aaf37a12fa9fe8394c9670c84dbd88995f3903b
blackbox24 <nicardid@gmail.com>
2025-04-05 09:47:31 +0000

    chore: added auth context provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext.jsx";
 import "./index.css";
 import App from "./App.jsx";
+// import "./utils/api.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
+    <AuthProvider>
       <App />
-    </Router>
+    </AuthProvider>
   </StrictMode>
 );
